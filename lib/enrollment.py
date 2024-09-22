26849c48acde330877c9f5e9d7f1a0834d7ee71d
SultanTipa1 <josephngumbaugush@gmail.com>
2024-09-22 12:19:25 +0300

    Adds all files

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -3,6 +3,7 @@ class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -14,6 +15,77 @@ class Student:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    
+    def course_count(self):
+        return len(self._enrollments)
+
+    def set_grade(self, enrollment, grade):
+        if enrollment in self._enrollments:
+            self._grades[enrollment] = grade
+        else:
+            raise Exception("Enrollment not found.")
+
+    def aggregate_average_grade(self):
+        if not self._grades:
+            return 0.0  # Return 0 if there are no grades
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        average_grade = total_grades / num_courses
+        return average_grade
+
+
+class Course:
+    def __init__(self, title):
+        self.title = title
+        self._enrollments = []
+
+    def add_enrollment(self, enrollment):
+        if isinstance(enrollment, Enrollment):
+            self._enrollments.append(enrollment)
+        else:
+            raise TypeError("enrollment must be an instance of Enrollment")
+
+    def get_enrollments(self):
+        return self._enrollments.copy()
+
+
+class Enrollment:
+    all = []
+    
+    def __init__(self, student, course):
+        if isinstance(student, Student) and isinstance(course, Course):
+            self.student = student
+            self.course = course
+            self._enrollment_date = datetime.now()
+            type(self).all.append(self)
+        else:
+            raise TypeError("Invalid types for student and/or course")
+
+    def get_enrollment_date(self):
+        return self._enrollment_date
+
+    def course_count(self):
+        return len(self._enrollments)
+
+    def set_grade(self, enrollment, grade):
+        if enrollment in self._enrollments:
+            self._grades[enrollment] = grade
+        else:
+            raise Exception("Enrollment not found.")
+        
+    def aggregate_average_grade(self):
+        if not self._grades:
+            return 0.0  # Return 0 if there are no grades
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        average_grade = total_grades / num_courses
+        return average_grade
+
+
+
+
+
+
 
 class Course:
     def __init__(self, title):
@@ -45,3 +117,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+    
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
